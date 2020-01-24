57dd3c5d57ec3b8f75c73faf2d33a9f14448e60b
Diogo Santos <diogo.filipe.santos107@gmail.com>
2020-01-24 18:03:42 +0000

    Lab Vikings. Complete minus the bonus part

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,66 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
+  attack() {
+    return this.strength;
+  }
+  receiveDamage(damage) {
+    this.health -= damage;
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(health, strength);
+    this.name = name;
+  }
+  receiveDamage(damage) {
+    if ((this.health -= damage) > damage) {
+      return `${this.name} has received ${damage} points of damage`;
+    } else {
+      return `${this.name} has died in act of combat`;
+    }
+  }
+  battleCry() {
+    return 'Odin Owns You All!';
+  }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  receiveDamage(damage) {
+    if ((this.health -= damage) > 0) {
+      return `A Saxon has received ${damage} points of damage`;
+    } else {
+      return `A Saxon has died in combat`;
+    }
+  }
+  attack() {
+    return this.strength;
+  }
+}
 
 // War
-class War {}
+class War {
+  constructor() {
+    this.vikingArmy = [];
+    this.saxonArmy = [];
+  }
+
+  addViking(numberOfVikings) {
+    this.vikingArmy.push(numberOfVikings);
+  }
+
+  addSaxon(numberOfSaxons) {
+    this.saxonArmy.push(numberOfSaxons);
+  }
+  vikingAttack() {}
+
+  saxonAttack() {}
+
+  showStatus() {}
+}
